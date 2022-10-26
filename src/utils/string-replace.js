6ad3d966547e9a1445fa9339f5029c9a3c1591ae
henricker <henriquevieira@alu.ufc.br>
2022-10-26 10:51:55 -0300

    feat: should return first letter uppercase or lowercase

---
+++ src/utils/string-replace.js
@@ -0,0 +1,19 @@
+
+export class StringReplacer {
+    
+    static #transform(str, isUpperCase = true) {
+        if (str.length === 0) return ''
+        const [firstLetter, ...rest] = str;
+        const newFirstLetter = isUpperCase ? firstLetter.toUpperCase() : firstLetter.toLowerCase();
+
+        return newFirstLetter + rest.join('');
+    }
+
+    static firstLetterToUpperCase(str) {
+        return StringReplacer.#transform(str, true);
+    }
+
+    static firstLetterToLowerCase(str) {
+        return StringReplacer.#transform(str, false);
+    }
+}
