5a27c1b470148f4965633c9194eb2b418bf14b7e
henricker <henriquevieira@alu.ufc.br>
2022-10-26 10:50:55 -0300

    chore: add initial setup of project

---
+++ jest.config.mjs
@@ -0,0 +1,39 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ */
+
+export default {
+  
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
+  // força um coverage para todos os arquivos
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/index.js"
+  ],
+   coverageThreshold: {
+     global: {
+       branch: 100,
+       functions: 100,
+       lines: 100,
+       statements:100
+     }
+   },
+ 
+  maxWorkers: "50%",
+  testEnvironment: "node",
+  watchPathIgnorePatterns: [
+    "node_modules"
+  ],
+  transformIgnorePatterns: ["node_modules"],
+  testMatch: [
+    '**/__tests__/**/*.test.js',
+    '!**/src/**'
+  ]
+};
